e20636bed7916aa9f3b89381e61752d4bc1acd1c
Rui Maranhao <rui@computer.org>
2018-10-28 00:00:34 +0100

    more changes; not working

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import './react-accelerometer';
+import ReactAccelerometer from './react-accelerometer';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
